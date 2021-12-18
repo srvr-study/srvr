61da40c386d3c17bf7919bb50c855607e6f1837d
Alencion <rhqnrdl625@gmail.com>
2021-12-18 21:44:57 +0900

    chore: 프로젝트 초기화 - #1
    기본 spring 프로젝트 초기화. 추가 디펜던시는
    srvr-main 폴더 안에 build.gradle 에 추가해서
    개발하세요.

---
+++ srvr-main/src/main/java/kr/kro/srvrstudy/srvr_main/SrvrMainApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.srvrstudy.srvr_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvrMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrvrMainApplication.class, args);
+    }
+
+}
