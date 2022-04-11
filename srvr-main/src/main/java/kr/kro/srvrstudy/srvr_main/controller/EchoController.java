44ddd1b9291b464e69fced9f1429d6e6c9733a75
이정준/협업서비스개발그린팀/ND <jeongjun.lee@nhndooray.com>
2022-04-11 22:33:31 +0900

    feat: test용 main server echo controller 추가
    - main-client 만들기 위한 main server에 echo controller 추가
    - main server에 srvr-common 의존 모듈 추가

---
+++ srvr-main/src/main/java/kr/kro/srvrstudy/srvr_main/controller/EchoController.java
@@ -0,0 +1,24 @@
+package kr.kro.srvrstudy.srvr_main.controller;
+
+import kr.kro.srvrstudy.srvr_common.api.response.ApiResponse;
+import kr.kro.srvrstudy.srvr_common.api.response.SuccessResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EchoController {
+
+    @PostMapping("/")
+    public ApiResponse<String> post(@RequestBody String body) {
+        return new SuccessResponse<>(body);
+    }
+
+    @GetMapping("/{path}")
+    public ApiResponse<String> getPath(@PathVariable String path) {
+        return new SuccessResponse<>(path);
+    }
+
+}
